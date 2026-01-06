821759b2dd840b813823813cb4ec28de2e30216c
NavineDevs <navinedevs@proton.me>
2026-01-06 12:46:23 +0900

    Update uv.sw.js

--- public/uv/uv.sw.js
+++ public/uv/uv.sw.js
@@ -1,24 +1,14 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
-importScripts("uv.bundle.js");
-importScripts("uv.config.js");
-importScripts(__uv$config.sw || "uv.sw.js");
+/* global UVServiceWorker, __uv$config */
+
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
 
 const uv = new UVServiceWorker();
 
-async function handleRequest(event) {
-	if (uv.route(event)) {
-		return await uv.fetch(event);
-	}
-
-	return await fetch(event.request);
-}
-
 self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+  if (uv.route(event)) {
+    event.respondWith(uv.fetch(event));
+  } else {
+    event.respondWith(fetch(event.request));
+  }
 });
